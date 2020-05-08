14ad82177e16b71859f9a0a4c275b6e47aef340d
Brett Heithold <baheithold@crimson.ua.edu>
2020-05-07 20:03:36 -0500

    Removed unnecessary imports

--- Algorithm Visualizer/src/algorithmVisualizer/VisualizerWindow.java
+++ Algorithm Visualizer/src/algorithmVisualizer/VisualizerWindow.java
@@ -1,8 +1,6 @@
 package algorithmVisualizer;
 
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
