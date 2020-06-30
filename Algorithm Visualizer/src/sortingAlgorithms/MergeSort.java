7db2489a6c386c2181feecc3f8aa66495023eec4
Brett Heithold <baheithold@crimson.ua.edu>
2020-06-30 18:20:51 -0500

    Implemented MergeSort algorithm

--- Algorithm Visualizer/src/sortingAlgorithms/MergeSort.java
+++ Algorithm Visualizer/src/sortingAlgorithms/MergeSort.java
@@ -24,8 +24,10 @@ public class MergeSort implements Runnable {
 
 			@Override
 			protected Void doInBackground() throws Exception {
-				sort(array.getArray(), 0, array.length());
-				publish();
+				sort(array.getArray(), 0, array.length() - 1);
+				for (int i = 0; i < array.length(); i++) {
+					System.out.println(array.getValue(i));
+				}
 				return null;
 			}
 
@@ -33,7 +35,7 @@ public class MergeSort implements Runnable {
 			protected void process(List<Void> chunks) {
 				array.repaint();
 			}
-			
+
 			private void merge(int arr[], int l, int m, int r) {
 				// Calculate partition sizes
 				int nLeft = m - l + 1;
@@ -43,6 +45,14 @@ public class MergeSort implements Runnable {
 				int leftPartition[] = new int[nLeft];
 				int rightPartition[] = new int [nRight];
 				
+				// Copy data to partition arrays
+				for (int i = 0; i < nLeft; i++) {
+					leftPartition[i] = arr[l + i];
+				}
+				for (int j = 0; j < nRight; j++) {
+					rightPartition[j] = arr[m + 1 + j];
+				}
+				
 				// Merge left and right partitions
 				int i = 0;
 				int j = 0;
@@ -50,23 +60,27 @@ public class MergeSort implements Runnable {
 				while (i < nLeft && j < nRight) {
 					if (leftPartition[i] <= rightPartition[j]) {
 						arr[k] = leftPartition[i];
+						System.out.println("Comparing " + leftPartition[i] + " and " + rightPartition[j]);
 						i++;
 					}
 					else {
 						arr[k] = rightPartition[j];
 						j++;
 					}
+					System.out.println("Merged: " + arr[k]);
 					k++;
 				}
 				
 				// Either left partition or right partition is now empty, merge the remaining elements
 				while (i < nLeft) {
 					arr[k] = leftPartition[i];
+					System.out.println("Merged: " + arr[k]);
 					i++;
 					k++;
 				}
 				while (j < nRight) {
 					arr[k] = rightPartition[j];
+					System.out.println("Merged: " + arr[k]);
 					j++;
 					k++;
 				}
@@ -89,7 +103,6 @@ public class MergeSort implements Runnable {
 		};
 		
 		workerThread.execute();
-		
 	}
 
 }
