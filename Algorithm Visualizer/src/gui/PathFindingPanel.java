21f181b2261786e28e74dd00da0098cf1e91242a
Brett Heithold <baheithold@crimson.ua.edu>
2020-07-25 20:19:45 -0500

    Working on drawing the grid

--- Algorithm Visualizer/src/gui/PathFindingPanel.java
+++ Algorithm Visualizer/src/gui/PathFindingPanel.java
@@ -1,5 +1,9 @@
 package gui;
 
+import java.awt.Graphics;
+
+import pathFindingAlgorithms.Grid;
+
 /**
  * @author Brett Heithold
  *
@@ -7,9 +11,20 @@ package gui;
 public class PathFindingPanel extends VisualizationPanel {
 	private static final long serialVersionUID = 1L;
 	private String algorithmName;
+	private Grid grid;
 
 	public PathFindingPanel(String algorithmName) {
 		this.algorithmName = algorithmName;
+		this.grid = new Grid(this, this.windowWidth / 20, this.windowHeight / 20);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		for (int i = 0; i <= this.windowWidth; i += 20) {
+			for (int j = 0; j <= this.windowHeight - 60; j += 20) {
+				g.drawRect(i, j, 20, 20);
+			}
+		}
 	}
 	
 }
