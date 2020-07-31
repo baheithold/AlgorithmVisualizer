a11eda0cee2a4b6f62b3e9fc19ce3f089761e443
Brett Heithold <baheithold@crimson.ua.edu>
2020-07-31 16:49:53 -0500

    Added dfs to switch statement

--- Algorithm Visualizer/src/gui/PathFindingPanel.java
+++ Algorithm Visualizer/src/gui/PathFindingPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseListener;
 
 import pathFindingAlgorithms.AStar;
 import pathFindingAlgorithms.BreadthFirstSearch;
+import pathFindingAlgorithms.DepthFirstSearch;
 import pathFindingAlgorithms.Grid;
 import pathFindingAlgorithms.GridNode;
 import pathFindingAlgorithms.PathFindingAlgorithm;
@@ -40,6 +41,9 @@ public class PathFindingPanel extends VisualizationPanel implements MouseListene
 				break;
 			case "bfs":
 				pathFindingAlgorithm = new BreadthFirstSearch(this);
+				break;
+			case "dfs":
+				pathFindingAlgorithm = new DepthFirstSearch(this);
 				break;
 			default:
 				break;
