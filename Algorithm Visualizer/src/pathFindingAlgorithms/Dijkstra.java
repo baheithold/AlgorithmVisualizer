8219eb40eb4afb2861302a50e3c9b883ec7750b6
Brett Heithold <baheithold@crimson.ua.edu>
2020-08-04 14:25:29 -0500

    Started working on Dijkstra class

---
+++ Algorithm Visualizer/src/pathFindingAlgorithms/Dijkstra.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package pathFindingAlgorithms;
+
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
+import gui.PathFindingPanel;
+
+/**
+ * @author Brett Heithold
+ *
+ */
+public class Dijkstra extends PathFindingAlgorithm implements Runnable {
+	SwingWorker<Void, Void> workerThread;
+	
+	public Dijkstra(PathFindingPanel panel) {
+		super(panel);		
+	}
+
+	@Override
+	public void run() {
+		workerThread = new SwingWorker<Void, Void>() {
+
+			@Override
+			protected Void doInBackground() throws Exception {
+				
+				// if start or end node is missing, return
+				if (!verifyStartEndNodesExist()) {
+					setRunning(false);
+					return null;
+				}
+				
+				System.out.println("Path does not exist!");
+				publish();
+				setRunning(false);
+				return null;
+			}
+			
+			@Override
+			protected void process(List<Void> chunks) {
+				panel.repaint();
+			}
+			
+		};
+		
+		workerThread.execute();
+		
+	}
+
+	@Override
+	public void runPathFindingAlgorithm() {
+		setRunning(true);
+		SwingUtilities.invokeLater(this);
+	}
+
+	@Override
+	public void killPathFindingAlgorithm() {
+		setRunning(false);
+		workerThread.cancel(true);
+	}
+	
+}
