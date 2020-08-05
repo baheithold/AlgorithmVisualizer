531572c1eb937ff11f66eb2a9b458cbc60a3a85a
Brett Heithold <baheithold@crimson.ua.edu>
2020-08-05 15:44:31 -0500

    Added DFS and dijkstra to graph dropdown menu

--- Algorithm Visualizer/src/gui/GraphMenu.java
+++ Algorithm Visualizer/src/gui/GraphMenu.java
@@ -18,7 +18,7 @@ public class GraphMenu extends JMenu implements ActionListener {
 	private JFrame window;
 	
 	// Graph Menu Items
-	private JMenuItem primJMenuItem, kruskalJMenuItem, bellmanfordJMenuItem;
+	private JMenuItem dfsJMenuItem, dijkstraJMenuItem, primJMenuItem, kruskalJMenuItem, bellmanfordJMenuItem;
 	
 	public GraphMenu(JFrame window) {
 		this.window = window;
@@ -29,6 +29,10 @@ public class GraphMenu extends JMenu implements ActionListener {
 	
 	private void initializeGraphMenuItems() {
 
+		dfsJMenuItem = new JMenuItem("Depth First Search");
+		dfsJMenuItem.addActionListener(this);
+		dijkstraJMenuItem = new JMenuItem("Dijkstra");
+		dijkstraJMenuItem.addActionListener(this);
 		primJMenuItem = new JMenuItem("Prim");
 		primJMenuItem.addActionListener(this);
 		kruskalJMenuItem = new JMenuItem("Kruskal");
@@ -38,6 +42,8 @@ public class GraphMenu extends JMenu implements ActionListener {
 	}
 	
 	private void constructGraphMenu() {
+		add(dfsJMenuItem);
+		add(dijkstraJMenuItem);
 		add(primJMenuItem);
 		add(kruskalJMenuItem);
 		add(bellmanfordJMenuItem);
@@ -46,7 +52,15 @@ public class GraphMenu extends JMenu implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String algorithmName;
-		if (e.getSource() == primJMenuItem) {
+		if (e.getSource() == dfsJMenuItem) {
+			algorithmName = "dfs";
+			window.setTitle("Depth First Search");
+		}
+		else if (e.getSource() == dijkstraJMenuItem) {
+			algorithmName = "dijkstra";
+			window.setTitle("Dijkstra");
+		}
+		else if (e.getSource() == primJMenuItem) {
 			algorithmName = "prim";
 			window.setTitle("Prim");
 		}
