620a2247e6239e063664ba2fe0f6a1d266c41ebd
Brett Heithold <baheithold@crimson.ua.edu>
2020-08-13 19:54:38 -0500

    Created GraphAlgorithm class

---
+++ Algorithm Visualizer/src/graphAlgorithms/GraphAlgorithm.java
@@ -0,0 +1,22 @@
+package graphAlgorithms;
+
+import algorithmVisualizer.Algorithm;
+
+/**
+ * @author Brett Heithold
+ *
+ */
+public class GraphAlgorithm extends Algorithm{
+	
+	public GraphAlgorithm() {
+	}
+
+	@Override
+	public void runAlgorithm() {
+	}
+
+	@Override
+	public void killAlgorithm() {
+	}
+
+}
