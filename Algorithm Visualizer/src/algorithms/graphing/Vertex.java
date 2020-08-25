1b4b6fd8218fced8ceacd90bb8614e3820e97f7b
Brett Heithold <baheithold@crimson.ua.edu>
2020-08-25 00:02:29 -0500

    Added color constants and methods to Vertex class

--- Algorithm Visualizer/src/algorithms/graphing/Vertex.java
+++ Algorithm Visualizer/src/algorithms/graphing/Vertex.java
@@ -11,6 +11,15 @@ import java.awt.geom.Ellipse2D;
  */
 public class Vertex {
 	
+	// Color Constants
+	private final Color DEFAULT_COLOR = Color.darkGray;
+	private final Color START_COLOR = Color.green;
+	private final Color END_COLOR = Color.red;
+	private final Color SELECTED_COLOR = Color.blue;
+	
+	// Color
+	private Color color;
+	
 	private double diameter;
 	private double xPosition, yPosition;
 
@@ -18,6 +27,7 @@ public class Vertex {
 		this.diameter = diameter;
 		xPosition = xPos;
 		yPosition = yPos;
+		color = DEFAULT_COLOR;
 	}
 	
 	public double xCentered() {
@@ -32,10 +42,46 @@ public class Vertex {
 		return diameter / 2;
 	}
 	
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setDefault() {
+		color = DEFAULT_COLOR;
+	}
+	
+	public void setStart() {
+		color = START_COLOR;
+	}
+	
+	public void setEnd() {
+		color = END_COLOR;
+	}
+	
+	public void setSelected() {
+		color = SELECTED_COLOR;
+	}
+	
+	public boolean isDefault() {
+		return color == DEFAULT_COLOR;
+	}
+	
+	public boolean isStart() {
+		return color == START_COLOR;
+	}
+	
+	public boolean isEnd() {
+		return color == END_COLOR;
+	}
+	
+	public boolean isSelected() {
+		return color == SELECTED_COLOR;
+	}
+	
 	public void draw(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		Ellipse2D.Double circle = new Ellipse2D.Double(xCentered(), yCentered(), diameter, diameter);
-		g2d.setColor(Color.black);
+		g2d.setColor(color);
 		g2d.fill(circle);
 	}
 
