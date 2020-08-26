fb6184d04e1338e787ae3603cbc905ed9a6ed28d
Brett Heithold <baheithold@crimson.ua.edu>
2020-08-26 09:57:25 -0500

    Added more functionality to Edge class

--- Algorithm Visualizer/src/algorithms/graphing/Edge.java
+++ Algorithm Visualizer/src/algorithms/graphing/Edge.java
@@ -13,21 +13,25 @@ public class Edge {
 	
 	// Color Constants
 	private final Color DEFAULT_COLOR = Color.black;
+	private final Color SELECTED_COLOR = Color.magenta;
 	
 	// Edge Constants
-	private final int EDGE_THICKNESS = 3;
+	private final int DEFAULT_STROKE = 3;
+	private final int SELECTED_STROKE = 5;
 
 	private Vertex u;
 	private Vertex v;
 	private double weight;
 	
 	private Color color;
+	private int stroke;
 	
 	public Edge(Vertex u, Vertex v) {
 		this.u = u;
 		this.v = v;
 		this.weight = 0.0;
 		color = DEFAULT_COLOR;
+		stroke = DEFAULT_STROKE;
 	}
 	
 	public Edge(Vertex u, Vertex v, double w) {
@@ -35,6 +39,7 @@ public class Edge {
 		this.v = v;
 		this.weight = w;
 		color = DEFAULT_COLOR;
+		stroke = DEFAULT_STROKE;
 	}
 	
 	public Vertex getU() {
@@ -52,12 +57,26 @@ public class Edge {
 	public void setWeight(double w) {
 		weight = w;
 	}
+	
+	public void setDefault() {
+		color = DEFAULT_COLOR;
+		stroke = DEFAULT_STROKE;
+	}
+	
+	public void setSelected() {
+		color = SELECTED_COLOR;
+		stroke = SELECTED_STROKE;
+	}
 
 	public void draw(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
+		
+		// draw line from u to v
 		g2d.setColor(color);
-		g2d.setStroke(new BasicStroke(EDGE_THICKNESS));
+		g2d.setStroke(new BasicStroke(stroke));
 		g2d.drawLine((int)u.xCentered() + (int)u.radius(), (int)u.yCentered() + (int)u.radius(), (int)v.xCentered() + (int)u.radius(), (int)v.yCentered() + (int)u.radius());
+		
+		// reset stroke to 0
 		g2d.setStroke(new BasicStroke(0));
 	}
 	
