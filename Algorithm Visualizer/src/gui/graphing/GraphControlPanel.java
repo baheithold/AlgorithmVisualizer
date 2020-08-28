30199c85c2d38fe7287d42d4093493b49040f1b9
Brett Heithold <baheithold@crimson.ua.edu>
2020-08-28 03:10:25 -0500

    A random weight is generated for each edge when weighted is checked

--- Algorithm Visualizer/src/gui/graphing/GraphControlPanel.java
+++ Algorithm Visualizer/src/gui/graphing/GraphControlPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Random;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -297,6 +298,7 @@ public class GraphControlPanel extends JPanel implements ActionListener, ChangeL
 			else {
 				for (Edge edge : panel.getEdges()) {
 					edge.setWeighted(true);
+					edge.setWeight(new Random().nextInt(panel.getEdges().size() * 2));
 				}
 			}
 			panel.repaint();
